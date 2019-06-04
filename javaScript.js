b196cb4cba6229645a994fd5417b25a130be70b1
Woojin Wie <dnldnwls1123@gmail.com>
2019-06-04 10:31:30 +0900

    asd

--- javaScript.js
+++ javaScript.js
@@ -26,3 +26,11 @@ function goHome(){
 function goSecond(){
     location.replace("second.html");
 }
+
+function carGo(){
+    window.open("https://www.tesla.com");
+}
+
+function homeGo(){
+    window.open("https://www.airbnb.co.kr/rooms/369034?search_id=4ce2e542-9e0e-becf-aa88-9cad62ad6e99&federated_search_id=c1d63e69-4e31-43dc-9253-93218941919d");
+}
